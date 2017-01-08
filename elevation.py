b939d81e1331de752de6da5f6ddb9ebdcdca50b0
Michael Pollind <mpollind@gmail.com>
2017-01-08 12:20:14 -0800

    added altitude script

--- elevation.py
+++ elevation.py
@@ -1,13 +1,13 @@
-import googlemaps,json
-gmaps = googlemaps.Client(key='AIzaSyBkFhUMEuDdlSBXYp18Pmy7bMKwK0M7IrM')
 
-# passed address
-geocode_result = gmaps.geocode('814 Lantana Ave Brea, CA')
+from app.database import db
+from app.google_maps import google_maps
+# need to add column ALTER TABLE `AKZA-6UK256_ChapmanData_12072016` ADD ALTITUDE FLOAT NULL;
 
-#lng/lat
-lat = geocode_result[0]['geometry']['location']['lat']
-lng = geocode_result[0]['geometry']['location']['lng']
-print(lat)
-print(lng)
-elevation_result = gmaps.elevation((lat,lng))
-print(elevation_result[0]['elevation'])
+rows = db.query("SELECT * FROM `AKZA-6UK256_ChapmanData_12072016` WHERE ALTITUDE IS NULL");
+for row in rows:
+    id = row["ID"]
+    lat = row["PARCEL LEVEL LATITUDE"]
+    lng = row["PARCEL LEVEL LONGITUDE"]
+    elevation_result = google_maps.elevation((lat, lng))
+    print id
+    db.query('''UPDATE `AKZA-6UK256_ChapmanData_12072016` SET ALTITUDE = :elevation WHERE ID = :id''',id = id, elevation = elevation_result[0]['elevation'])
